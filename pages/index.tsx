6f8e060fa30e913d2582f5c660d9ae5b84ffd732
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-10-10 22:52:53 -0700

    limpiando proyecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { Button, Stack } from '@mantine/core'
-import { localStorageMethods } from '../classes/localStorageMethods';
+
 
 const Home = () => {
 
   return <Stack>
     <h1>Desde inicio</h1>
-    <Button onClick={() => localStorageMethods.setItem('table_food', undefined)} />
+
 
   </Stack>
 }
