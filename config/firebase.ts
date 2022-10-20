bea1163d4af39f1a374bdc2d4520fc178ae32804
itsaimejia <33639089+itsaimejia@users.noreply.github.com>
2022-10-19 17:58:16 -0700

    firebase ignore

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCXA6TtpPCpUDNYh9FUulLtXGP4dnCstO4",
-  authDomain: "smaedb-b59df.firebaseapp.com",
-  databaseURL: "https://smaedb-b59df-default-rtdb.firebaseio.com",
-  projectId: "smaedb-b59df",
-  storageBucket: "smaedb-b59df.appspot.com",
-  messagingSenderId: "226116553576",
-  appId: "1:226116553576:web:8f5893a22d3a14ef51a4dd",
-  measurementId: "G-8YDJFETQ8L"
-};
+  apiKey: "AIzaSyD0K-eGKF_nWMInHhJInbqFWCsiZgBWeaQ",
+  authDomain: "food-4eb80.firebaseapp.com",
+  databaseURL: "https://food-4eb80-default-rtdb.firebaseio.com",
+  projectId: "food-4eb80",
+  storageBucket: "food-4eb80.appspot.com",
+  messagingSenderId: "761828869602",
+  appId: "1:761828869602:web:bbef2940978c7ce9c065bb"
+}
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
